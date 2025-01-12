888fc12a8364cfc7a91fcb917076967963538c00
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-01-12 22:53:07 +0800

    jsx file for the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,23 @@
-
-export default function Header(){
+export default function Header() {
   return (
-    <header>
-      
+    <header className="header__container container">
+      <div className="logo__container">
+        <a href="#">
+          <img
+            src="/src/assets/logo.png"
+            className="logo__img"
+            alt="logo image"
+          />
+        </a>
+      </div>
+      <div className="right__content">
+        <a
+          href=""
+          className="login__link"
+        >
+          Log in
+        </a> 
+      </div>
     </header>
-  )
+  );
 }

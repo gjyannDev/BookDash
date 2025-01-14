6a7d32b8cf878ce02449f37bdc933cb765f016ac
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-01-14 10:18:07 +0800

    add the card.jsx and the card component in the app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './components/Header.jsx'
 import Hero from './components/Hero.jsx'
+import Card from './components/Card.jsx'
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      <Card />
     </>
   )
 }

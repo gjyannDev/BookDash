e01013e3592a52f4945a0525dac0292d619de00f
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-01-20 08:52:37 +0800

    added new file for the routes

---
+++ src/routes.jsx
@@ -0,0 +1,22 @@
+import App from './App'
+import Login from './components/Login'
+import Signup from './components/Signup'
+
+const routes = [
+  {
+    path: '/',
+    element: <App />
+  },
+  
+  {
+    path: 'login',
+    element: <Login />
+  },
+
+  {
+    path: 'signup',
+    element: <Signup />
+  }
+]
+
+export default routes;

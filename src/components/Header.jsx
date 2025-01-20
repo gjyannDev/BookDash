2623293ed0cdbe0850665bd89adaf6a0d68457d6
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-01-20 08:55:15 +0800

    changed the routes and also change achor tag with link provided by the react router

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,19 @@
+import { Link } from 'react-router-dom'
+
 export default function Header() {
   return (
     <header className="header__container container">
       <div className="logo__container">
-        <a href="#">
+        <Link to={'/'}>
           <img
-            src="/src/assets/logo.png"
-            className="logo__img"
-            alt="logo image"
-          />
-        </a>
+              src="/src/assets/logo.png"
+              className="logo__img"
+              alt="logo image"
+            />
+        </Link>
       </div>
       <div className="right__content">
-        <a
-          href=""
-          className="login__link"
-        >
-          Log in
-        </a> 
+        <Link to="login" className="login__link"> Login </Link>
       </div>
     </header>
   );

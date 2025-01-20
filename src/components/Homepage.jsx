ea8376c45d1b9cda1a8934bd1772a3c3c02ff4af
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-01-20 08:56:00 +0800

    Add the content of the homepage to the homepage.jsx file

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,13 @@
+import Header from './Header.jsx'
+import Hero from './Hero.jsx'
+import Card from './Card.jsx'
+
+export default function Homepage() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Card />
+    </>
+  )
+}
